62ade011e684eadd1d3725c9bcb6b9f9a0cc0da4
rex <rexpeters3n@gmail.com>
2025-08-22 14:48:44 -0400

    chore: fix output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,12 +31,6 @@ export default async function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-        
-        <div>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-          
-        </div>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
