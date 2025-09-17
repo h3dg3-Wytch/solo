aab66fdd838fd3976db3084225016b1feeab83f2
rex <rexpeters3n@gmail.com>
2025-09-17 14:12:33 -0400

    add in defaultPlotLineTable

---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+export enum THEMES {
+    ACTION,
+    SOCIAL,
+    PERSONAL,
+    TENSION,
+    MYSTERY
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,20 @@
 export function generateAdventureResult() {
     
 }
+
+export function rollDie(sides) {
+  if (sides < 1 || !Number.isInteger(sides)) {
+    throw new Error("Number of sides must be a positive integer.");
+  }
+
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+export function shuffle<T>(array: T[]): T[] {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
